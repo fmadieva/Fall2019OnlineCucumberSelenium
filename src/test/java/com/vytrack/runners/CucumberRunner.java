10843cc887bedc460005dc3bd238ba6195104ae8
fmadieva <fariza.madieva@gmail.com>
2020-04-15 19:29:39 -0500

    scenario in cucumber

--- src/test/java/com/vytrack/runners/CucumberRunner.java
+++ src/test/java/com/vytrack/runners/CucumberRunner.java
@@ -9,7 +9,8 @@ import org.junit.runner.RunWith;
         glue = "com/vytrack/step_definitions",
         features = "src/test/resources/features",
         dryRun = false,
-        strict = true
+        strict = false,
+        tags = "@driver"
 
 )
 public class CucumberRunner {
